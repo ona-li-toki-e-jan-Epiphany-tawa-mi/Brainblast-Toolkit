e6d913fd45f98f5079e690fd40c280ff6b47a3e2
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-06-16 19:58:43 -0400

    Applied shellcheck recommendations to config.sh

--- config.sh
+++ config.sh
@@ -18,36 +18,36 @@
 # Brainblast-Toolkit. If not, see <https://www.gnu.org/licenses/>.
 
 # Metadata.
-TOOLKIT_VERSION=0.1.0
+export TOOLKIT_VERSION=0.1.0
 
 # The size, in bytes, of the stack used to recall previous user inputs.
-HISTORY_STACK_SIZE=1028
+export HISTORY_STACK_SIZE=1028
 
 # The number of bytes to allocate for BASICfuck cell memory. Make this 30,000
 # cells maxiumum. If someone wants more they can always change it.
-C64_CELL_MEMORY_SIZE=30000
-C128_CELL_MEMORY_SIZE=28000
-PLUS4_CELL_MEMORY_SIZE=30000
-PET_CELL_MEMORY_SIZE=17500
-CX16_CELL_MEMORY_SIZE=25000
-ATARI_CELL_MEMORY_SIZE=25900
-ATARIXL_CELL_MEMORY_SIZE=28300
+export C64_CELL_MEMORY_SIZE=30000
+export C128_CELL_MEMORY_SIZE=28000
+export PLUS4_CELL_MEMORY_SIZE=30000
+export PET_CELL_MEMORY_SIZE=17500
+export CX16_CELL_MEMORY_SIZE=25000
+export ATARI_CELL_MEMORY_SIZE=25900
+export ATARIXL_CELL_MEMORY_SIZE=28300
 
 # Which file extension to use for generated binaries.
-C64_BINARY_FILE_EXTENSION=prg
-C128_BINARY_FILE_EXTENSION=prg
-PLUS4_BINARY_FILE_EXTENSION=prg
-PET_BINARY_FILE_EXTENSION=prg
-CX16_BINARY_FILE_EXTENSION=prg
-ATARI_BINARY_FILE_EXTENSION=com
-ATARIXL_BINARY_FILE_EXTENSION=com
+export C64_BINARY_FILE_EXTENSION=prg
+export C128_BINARY_FILE_EXTENSION=prg
+export PLUS4_BINARY_FILE_EXTENSION=prg
+export PET_BINARY_FILE_EXTENSION=prg
+export CX16_BINARY_FILE_EXTENSION=prg
+export ATARI_BINARY_FILE_EXTENSION=com
+export ATARIXL_BINARY_FILE_EXTENSION=com
 
 # Emulator commands. The binary to run will be appended to the end of the
 # command.
-C64_EMULATOR='x64 -silent'
-C128_EMULATOR='x128 -silent'
-PLUS4_EMULATOR='xplus4 -silent'
-PET_EMULATOR='xpet -silent'
-CX16_EMULATOR='x16emu -rom /usr/share/x16-rom/rom.bin -prg'
-ATARI_EMULATOR='atari800 > /dev/null -run'
-ATARIXL_EMULATOR='atari800 -xl > /dev/null -run'
+export C64_EMULATOR='x64 -silent'
+export C128_EMULATOR='x128 -silent'
+export PLUS4_EMULATOR='xplus4 -silent'
+export PET_EMULATOR='xpet -silent'
+export CX16_EMULATOR='x16emu -rom /usr/share/x16-rom/rom.bin -prg'
+export ATARI_EMULATOR='atari800 > /dev/null -run'
+export ATARIXL_EMULATOR='atari800 -xl > /dev/null -run'
